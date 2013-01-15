bf132c97b017d71de5acda8aec66327ee46fd24e
Terrell Russell <terrellrussell@gmail.com>
2013-01-15 16:54:20 -0500

    [#1134] whitespace changes, tab police - no functional changes

--- metrics/IrodsStatus.py
+++ metrics/IrodsStatus.py
@@ -13,81 +13,80 @@ from IrodsMetricAbstract import IrodsMetric
 # }
 
 def isIrodsServerUp(irods_path):
-	process_name = 'irodsServer'
-	ps = subprocess.Popen("ps -eaf | grep "+process_name,
-				shell=True, stdout=subprocess.PIPE)
-	output = ps.stdout.read()
-	ps.stdout.close
-	ps.wait()
+    process_name = 'irodsServer'
+    ps = subprocess.Popen("ps -eaf | grep "+process_name, shell=True, stdout=subprocess.PIPE)
+    output = ps.stdout.read()
+    ps.stdout.close
+    ps.wait()
 
-	if re.search(irods_path + '/server/bin/' + process_name, output) is None:
-		return False
-	else:
-		return True
+    if re.search(irods_path + '/server/bin/' + process_name, output) is None:
+        return False
+    else:
+        return True
 
 class IrodsStatus(IrodsMetric):
 
-	# This attribute lets Guinan know how often this metric 
-	# is collected (units are in minutes).
-	# If it is missing or <=0, this metric will never be collected.
-	# Also note that individual metrics can never be run more 
-	# frequently then Guinan is configured to run (via cron).
-	# This attribute is required.
-	runMeEvery = 10
+    # This attribute lets Guinan know how often this metric 
+    # is collected (units are in minutes).
+    # If it is missing or <=0, this metric will never be collected.
+    # Also note that individual metrics can never be run more 
+    # frequently then Guinan is configured to run (via cron).
+    # This attribute is required.
+    runMeEvery = 10
 
-	# This attribute specifies when the locally saved metric gets
-	# stale and should be updated in iRODS (units are minutes).
-	# This is useful when Guinan 'blacks out' for some reason and
-	# could not run at regular intervals.
-	expiration = 60
+    # This attribute specifies when the locally saved metric gets
+    # stale and should be updated in iRODS (units are minutes).
+    # This is useful when Guinan 'blacks out' for some reason and
+    # could not run at regular intervals.
+    expiration = 60
 
-	# Compares 'alive' status and then timestamp staleness.
-	def metricsMatch(self, live, local):
+    # Compares 'alive' status and then timestamp staleness.
+    def metricsMatch(self, live, local):
 
-		# No saved local metrics, therefore cannot match.
-		if local is None:
-			return False
+        # No saved local metrics, therefore cannot match.
+        if local is None:
+            return False
 
-		localM = local[self.__class__.__name__][0]['metric']
-		localTS = local[self.__class__.__name__][0]
+        localM = local[self.__class__.__name__][0]['metric']
+        localTS = local[self.__class__.__name__][0]
 
-		# Check to see if actual metric has changed
-		if (live['alive'] != localM['alive']):
-			return False
+        # Check to see if actual metric has changed
+        if (live['alive'] != localM['alive']):
+            return False
 
-		# Check to see if metric is stale
-		if (time.time() > (localTS['timestamp'] + (self.expiration*60))):
-			return False
+        # Check to see if metric is stale
+        if (time.time() > (localTS['timestamp'] + (self.expiration*60))):
+            return False
 
-		return True
+        return True
 
-	# This function is required.
-	def runMetric(self):
+    # This function is required.
+    def runMetric(self):
 
-		# Setup
-		logging.debug('Metric begin: %s' % self.__class__.__name__)
-		myMetric = dict() # {"alive": <True or False>}
+        # Setup
+        logging.debug('Metric begin: %s' % self.__class__.__name__)
+        myMetric = dict() # {"alive": <True or False>}
 
-		# Find out if server is up
-		irodsUp = False
-		self.mRods = GuinanCommon.MonitoredIrods()
-		irodsUp = isIrodsServerUp(self.mRods.homePath)
+        # Find out if server is up
+        irodsUp = False
+        self.mRods = GuinanCommon.MonitoredIrods()
+        irodsUp = isIrodsServerUp(self.mRods.homePath)
 
-		# Update metric
-		myMetric['alive'] = irodsUp
+        # Update metric
+        myMetric['alive'] = irodsUp
 
-		# Load the json file containing last collected update.
-		localMetrics = self.getLocalMetrics()
-		if self.metricsMatch(myMetric, localMetrics):
-			# Matched, nothing is new.
-			logging.debug('Metrics match: returning None')
-			myMetric =  None
-		else:
+        # Load the json file containing last collected update.
+        localMetrics = self.getLocalMetrics()
+        if self.metricsMatch(myMetric, localMetrics):
+            # Matched, nothing is new.
+            logging.debug('Metrics match: returning None')
+            myMetric =  None
+        else:
             # Did not match.
-			logging.debug('Metrics need to be updated')
+            logging.debug('Metrics need to be updated')
 
-		# Cleanup
-		logging.info('Metric complete: %s' % self.__class__.__name__)
+        # Cleanup
+        logging.info('Metric complete: %s' % self.__class__.__name__)
 
         # Return
-		return myMetric
+        return myMetric
--- metrics/TestMetric.py
+++ metrics/TestMetric.py
@@ -6,33 +6,33 @@ from IrodsMetricAbstract import IrodsMetric
 
 class TestMetric(IrodsMetric):
 
-	# This attribute lets Guinan know how often this metric
-	# is collected (units are in minutes).
-	# If it is missing or <=0, this metric will never be collected.
-	# Also note that individual metrics can never be run more
-	# frequently then Guinan is configured to run (via cron).
-	# This attribute is required.
-	runMeEvery = 1
+    # This attribute lets Guinan know how often this metric
+    # is collected (units are in minutes).
+    # If it is missing or <=0, this metric will never be collected.
+    # Also note that individual metrics can never be run more
+    # frequently then Guinan is configured to run (via cron).
+    # This attribute is required.
+    runMeEvery = 1
 
-	# This function is required.
-	# Calculate/Collect/Generate the metric and return a dict() as shown.
-	def runMetric(self):
+    # This function is required.
+    # Calculate/Collect/Generate the metric and return a dict() as shown.
+    def runMetric(self):
 
-		logging.debug('Running %s metrics' % self.__class__.__name__)
+        logging.debug('Running %s metrics' % self.__class__.__name__)
 
-		# Placeholder for the test metric
-		myMetric = dict()
-		myMetric["message"] = "hello there"
+        # Placeholder for the test metric
+        myMetric = dict()
+        myMetric["message"] = "hello there"
 
-		# Additional metric info used for display in Scotty
-		myMetric[self.getDisplayName()] = "Test Metric"
-		myMetric[self.getDescription()] = "This is just a test."
+        # Additional metric info used for display in Scotty
+        myMetric[self.getDisplayName()] = "Test Metric"
+        myMetric[self.getDescription()] = "This is just a test."
 
-		# Debug message
-		logging.debug('%s: returning metric to Guinan: %s' % (self.__class__.__name__, myMetric))
+        # Debug message
+        logging.debug('%s: returning metric to Guinan: %s' % (self.__class__.__name__, myMetric))
 
-		# Return the dict() holding the metric value.
-		#
-		# Or return None if you do not want the metric
-		# to be saved for some reason.
-		return myMetric
+        # Return the dict() holding the metric value.
+        #
+        # Or return None if you do not want the metric
+        # to be saved for some reason.
+        return myMetric
